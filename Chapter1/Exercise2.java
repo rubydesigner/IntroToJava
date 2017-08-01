b4f5f0445d1336b9a150737233c93113e0d979c5
Megan Bullington <megan_bullington@me.com>
2017-08-01 11:42:09 -0500

    Create Exercise2.java

---
+++ Chapter1/Exercise2.java
@@ -0,0 +1,18 @@
+/******************************************
+*  Name: Megan Bullington                 *
+*  Date: August 1, 2017                   *
+*                                         *
+*  1.2 (Display five messages) Write a    *
+*  program that displays "Welcome to      *
+*  Java" five times.                      *
+******************************************/
+
+public class Exercise2 {
+  public static void main(String[] args) {
+    System.out.println("Welcome to Java");
+    System.out.println("Welcome to Java");
+    System.out.println("Welcome to Java");
+    System.out.println("Welcome to Java");
+    System.out.println("Welcome to Java");
+  }
+}
