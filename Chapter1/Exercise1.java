f5831b7bd9e11b5ed5a1d0e3377bafbc55d62438
Megan Bullington <megan_bullington@me.com>
2017-07-31 21:18:39 -0500

    Update Exercise1.java

--- Chapter1/Exercise1.java
+++ Chapter1/Exercise1.java
@@ -1,3 +1,13 @@
+/******************************************
+*  Name: Megan Bullington                 *
+*  Date: July 31, 2017                    *
+*                                         *
+*  1.1 (Display three messages) Write a   *
+*  program that displays "Welcome to      *
+*  Java", "Welcome to Computer Science",  *
+*  and "Programming is fun"               *
+******************************************/
+
 public class Exercise1 {
   public static void main(String[] args) {
     System.out.println("Welcome to Java");
